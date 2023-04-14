49b87875bc2070a143e9d26329897aa107a597ac
oriverk <oriveryk@gmail.com>
2023-04-15 01:13:18 +0900

    feat: add layout components

---
+++ src/components/Hero.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+import GithubIcon from "./icons/GithubIcon.svelte";
+import TwitterIcon from "./icons/TwitterIcon.svelte";
+import PencilIcon from "./icons/PencilIcon.svelte";
+</script>
+
+<div class="hero">
+  <h1>Kawano Yudai</h1>
+  <p><span class="text-gradient">Agr.</span> → ? / Bicycle</p>
+  <div class="links">
+    <a href="/#" target="_blank" rel="noopener noreferrer">
+      <GithubIcon />
+      <span class="sr-only">GitHub link</span>
+    </a>
+    <a href="/#" title="blog">
+      <TwitterIcon />
+      <span class="sr-only">Twitter link</span>
+    </a>
+    <a href="/#" title="cv">
+      <PencilIcon />
+      <span class="sr-only">cv link</span>
+    </a>
+  </div>
+</div>
+
+<style lang="scss">
+.hero {
+  margin-inline: auto;
+  width: 60ch;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  font-size: 3rem;
+  font-weight: 800;
+  margin: 0;
+}
+
+p {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+
+.text-gradient {
+  background-image: var(--accent-gradient);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-size: 400%;
+  // background-position: 0%;
+}
+
+.links {
+  display: flex;
+  gap: .5rem;
+}
+
+.links a {
+  // padding: .625rem;
+  width: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  aspect-ratio: 1;
+  border-radius: .5rem;
+  transition: background-color 100ms ease-in;
+  background-color: rgb(var(--color-hover));
+
+  &:hover {
+    background-color: rgba(0 0 0 / .3);
+  }
+}
+
+.links :global(svg) {
+  width: 2rem;
+  aspect-ratio: 1;
+}
+</style>
