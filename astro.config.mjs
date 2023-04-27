26aacc8873164067446f40646aa1da7297806310
oriverk <oriveryk@gmail.com>
2023-04-27 10:57:51 +0900

    feat: enable google analytics with @astrojs/partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
+// https://www.kevinzunigacuellar.com/blog/google-analytics-in-astro/
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
+import partytown from "@astrojs/partytown";
 import gfm from 'remark-gfm'
 import comment from 'remark-comment'
 
@@ -7,7 +9,15 @@ import { rehypeAnchor, rehypeFigure } from './src/utils/markdown'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [
+    svelte(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["datalayer.push"],
+      }
+    })
+  ],
   markdown: {
     remarkPlugins: [gfm, comment],
     rehypePlugins: [rehypeAnchor, rehypeFigure],
--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,7 @@
 
 interface ImportMetaEnv {
   readonly GITHUB_TOKEN: string
+  readonly PUBLIC_GA_MEASUREMENT_ID: string
 }
 
 interface ImportMeta {
