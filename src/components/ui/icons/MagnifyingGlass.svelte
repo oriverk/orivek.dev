6bc64de036993ec9ae6087c4fb587192360a3e31
oriverk <oriveryk@gmail.com>
2023-06-15 23:26:44 +0900

    feat: add algolia search for blog page

---
+++ src/components/ui/icons/MagnifyingGlass.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let className: string = ''
+  export let title: string = ''
+  export let fill: string = 'none'
+  export let stroke: string = 'currentColor'
+</script>
+
+<svg
+  viewBox="0 0 24 24"
+  xmlns="http://www.w3.org/2000/svg"
+  stroke-width="1.5"
+  {stroke}
+  {fill}
+  class={className}
+>
+  {#if title}
+    <title>{title}</title>
+  {/if}
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+  />
+</svg>
---
+++ src/types/algolia.ts
@@ -0,0 +1,10 @@
+export type AlgoliaBlog = {
+  objectID: string
+  title: string
+  create: string
+  update: string
+  tags: string[]
+  content: string
+  id: string
+  published: boolean
+}
