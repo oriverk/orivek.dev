4f5c75aa0b360c6f0511585587379db30bc1a683
oriverk <oriveryk@gmail.com>
2024-03-23 16:41:14 +0900

    chore: add packages to parse markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,45 @@
 // https://www.kevinzunigacuellar.com/blog/google-analytics-in-astro/
 import { defineConfig } from 'astro/config';
+// import partytown from "@astrojs/partytown";
 import svelte from "@astrojs/svelte";
-import partytown from "@astrojs/partytown";
+import mdx from "@astrojs/mdx";
+import remarkMath from "remark-math";
 import remarkComment from 'remark-comment';
-import { rehypeAnchor, rehypeFigure } from './src/utils/markdown';
+import remarkGithubAlerts from "remark-github-alerts";
+import rehypeKatex from "rehype-katex";
+import { remarkFencedCodeBlock, rehypeAnchor, rehypeFigure } from "./src/utils/markdown";
+import expressiveCode from "astro-expressive-code";
+import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   publicDir: "./public",
   integrations: [
-    svelte(),
     partytown({
       config: {
-        forward: ["datalayer.push"]
+        forward: ["datalayer.push"],
+      },
+    }),
+    svelte(),
+    // NOTE: expressiveCode must be before mdx
+    expressiveCode({
+      themes: ["github-dark"],
+      plugins: [pluginLineNumbers()],
+      defaultProps: {
+        wrap: false
+      },
+      frames: {
+        showCopyToClipboardButton: true,
+        removeCommentsWhenCopyingTerminalFrames: true
       }
-    })
+    }),
+    mdx()
   ],
   markdown: {
-    remarkPlugins: [remarkComment],
-    rehypePlugins: [rehypeAnchor, rehypeFigure]
+    remarkPlugins: [remarkComment, remarkMath, remarkFencedCodeBlock, remarkGithubAlerts],
+    rehypePlugins: [rehypeAnchor, rehypeFigure, rehypeKatex],
+    gfm: true
   }
 });
