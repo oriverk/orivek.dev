8f5d8e6bad0f251fc4e4e22118d683c86db638ec
oriverk <oriveryk@gmail.com>
2024-04-19 16:33:53 +0900

    feat: create and save image function at /api/og/ route

---
+++ src/utils/hashStringToSHA256.ts
@@ -0,0 +1,8 @@
+export async function hashStringToSHA256(str: string): Promise<string> {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(str);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+  return hashHex;
+}
