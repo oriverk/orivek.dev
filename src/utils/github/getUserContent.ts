3e7ebdc960c4f51795f398c3ee63aa458fa40c50
oriverk <oriveryk@gmail.com>
2024-04-21 21:57:20 +0900

    fix: fix embed content images

--- src/utils/github/getUserContent.ts
+++ src/utils/github/getUserContent.ts
@@ -1,15 +1,31 @@
+import fs from "node:fs";
 import type { UserContent } from "@/types/github";
-import contributionsJson from "../../../.contents/contributions.json";
-import repositoryJson from "../../../.contents/repository.json";
+import type { ContributionsCollection } from "@octokit/graphql-schema";
+
+const repositoryJsonPath = "./.contents/repository.json";
+const contributionsJsonPath = "./.contents/contributions.json";
+
+if (!fs.existsSync(contributionsJsonPath)) {
+  fs.writeFileSync(contributionsJsonPath, "{}");
+}
+
+if (!fs.existsSync(repositoryJsonPath)) {
+  fs.writeFileSync(repositoryJsonPath, "[]");
+}
 
 export function getUserContent(): UserContent {
+  const repositoryJson: UserContent["repositoryItems"] = JSON.parse(
+    fs.readFileSync(repositoryJsonPath, { encoding: "utf-8" }),
+  );
+  const contributionsJson: Pick<
+    ContributionsCollection,
+    "contributionCalendar"
+  > = JSON.parse(fs.readFileSync(contributionsJsonPath, { encoding: "utf-8" }));
+
   const repositoryItems: UserContent["repositoryItems"] = repositoryJson.filter(
     (repo) => !repo.isFork,
   );
-  const { contributionCalendar } = contributionsJson as unknown as Pick<
-    UserContent,
-    "contributionCalendar"
-  >;
+  const { contributionCalendar } = contributionsJson;
 
   return {
     repositoryItems,
