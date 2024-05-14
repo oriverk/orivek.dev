5c2ff31defa723543f4035f01b097a589039a3e7
oriverk <oriveryk@gmail.com>
2024-05-15 08:55:49 +0900

    fix: fix envs

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,11 +3,7 @@
 
 interface ImportMetaEnv {
   readonly SECRET_GITHUB_PERSONAL_ACCESS_TOKEN: string;
-  readonly PUBLIC_GA_MEASUREMENT_ID: string;
 
-  readonly PUBLIC_ALGOLIA_APP_ID: string;
-  readonly PUBLIC_ALGOLIA_SEARCH_KEY: string;
-  readonly PUBLIC_ALGOLIA_INDEX_BLOG: string;
   readonly SECRET_ALGOLIA_ADMIN_KEY: string;
 }
 
